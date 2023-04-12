cd7b1192e337e6dbdafed158d215d0c0cacc0a4a
Janaina Garcia Franquis <HA-GarciJan01@electrolux.com>
2023-04-11 22:40:37 -0300

    Solução Desafio Hospedagem Hotel

---
+++ Models/Reserva.cs
@@ -0,0 +1,62 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite?.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes
+            return Hospedes.Count;
+        }
+
+        public decimal? CalcularValorDiaria()
+        {
+            // TODO: Retorno valor da diária
+            decimal? valor = DiasReservados * Suite? .ValorDiaria;
+
+            //  // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.90M;
+            }
+            return valor;
+        }
+    }
+    // Construtor que chama o construtor da classe genérica
+    class Excecao : Exception
+    {
+        public Excecao(string mensagem) : base(mensagem)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1", sobrenome: "Teste1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2", sobrenome: "Teste2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3", sobrenome: "Teste3");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+hospedes.Add(p3);
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 10, valorDiaria: 90);
+Suite suite2 = new Suite(tipoSuite: "Luxo", capacidade: 3, valorDiaria: 70);
+Suite suite3 = new Suite(tipoSuite: "Standard", capacidade: 4, valorDiaria: 50);
+//Suite suite4 = new Suite(tipoSuite: "Comum", capacidade: 5, valorDiaria: 30);
+
+
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 5);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine("Suite: {0} - Valor diária: {1}", suite.TipoSuite, suite.ValorDiaria);
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
